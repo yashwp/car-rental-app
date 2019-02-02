ef066af11a0a4b65361b5ceb0e5e11fc68e932e5
Yashwardhan Pauranik <pauranik51@gmail.com>
2019-02-02 18:03:19 +0530

    filter pipe added

--- src/app/shared/pipes/pipes.module.ts
+++ src/app/shared/pipes/pipes.module.ts
@@ -1,13 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {FilterPipe} from './filter.pipe';
 import {OrderByPipe} from './order-by.pipe';
 
 @NgModule({
-  declarations: [OrderByPipe],
+  declarations: [OrderByPipe, FilterPipe],
   imports: [
     CommonModule
   ],
-  exports: [OrderByPipe]
+  exports: [OrderByPipe, FilterPipe]
 })
 export class PipesModule {
 }
